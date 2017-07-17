ef048ab507553b5edea269a68f9b2184ccd527a1
Paul Zilewitsch <pzilewitsch@proficom.de>
2017-07-17 15:06:15 +0200

    change

--- Test03/src/test/java/testcases/TestCaculator.java
+++ Test03/src/test/java/testcases/TestCaculator.java
@@ -3,6 +3,7 @@ package testcases;
 import static org.junit.Assert.*;
 
 import java.net.URL;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.*;
@@ -11,7 +12,9 @@ import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestCaculator 
 {
@@ -53,108 +56,113 @@ public class TestCaculator
 		    driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
 	  }
 	  
-	  @Test  
-	  public void testBaufinanzierung_Antrag_Start() throws Exception 
-	  {
-		// Store the current window handle
-		  winHandleBefore = driver.getWindowHandle();
+	@Test(timeout=1000000000)
+  public void testBaufinanzierung_Antrag() throws Exception {
 
-		// Perform the click operation that opens new window
-		driver.findElement(By.cssSelector("li.flex-active-slide > a.center.top")).click();
-		
-		// Switch to new window opened
-		for(String winHandle : driver.getWindowHandles()){
-		    driver.switchTo().window(winHandle);
-		}
-		
-		// Page : Baufinanzierung Onlineantrag -Start
-		
-		assertEquals("Commerzbank Finanzierungsanfrage", driver.getTitle());
-	    new Select(driver.findElement(By.id("cse.venture.reason"))).selectByVisibleText("Kauf eines Neubaus vom Bauträger");
-	    new Select(driver.findElement(By.id("cse.estate.combiType"))).selectByVisibleText("Eigentumswohnung");
-	    new Select(driver.findElement(By.id("cse.estate.utilization"))).selectByVisibleText("selbst bewohnen");
-	    if (!driver.findElement(By.id("radioButton_cse.estate.notfound_0")).isSelected()) {
-	      driver.findElement(By.id("radioButton_cse.estate.notfound_0")).click();
-	    };
-	    new Select(driver.findElement(By.id("cse.estate.buyTimeline"))).selectByVisibleText("");
-	    driver.findElement(By.id("cse.estate.zip")).clear();
-	    driver.findElement(By.id("cse.estate.zip")).sendKeys("01097");
-	    driver.findElement(By.id("cse.venture.priceBuilding")).clear();
-	    driver.findElement(By.id("cse.venture.priceBuilding")).sendKeys("260000");
-	    driver.findElement(By.id("cse.venture.originalFunding.loans[1].amount")).clear();
-	    driver.findElement(By.id("cse.venture.originalFunding.loans[1].amount")).sendKeys("50000");
-	    new Select(driver.findElement(By.id("cse.venture.originalFunding.loans[1].maturity"))).selectByVisibleText("10 Jahre");
-	    if (!driver.findElement(By.name("amortType")).isSelected()) {
-	      driver.findElement(By.name("amortType")).click();
-	    };
-	    	 
-	    driver.findElement(By.id("cse.venture.originalFunding.loans[1].amortisation")).clear();
-	    driver.findElement(By.id("cse.venture.originalFunding.loans[1].amortisation")).sendKeys("2");
-	     
-	    driver.findElement(By.id("togglelinkoptionalOther")).click();
-	       
-	    driver.findElement(By.id("cse.estate.city")).clear();
-	    driver.findElement(By.id("cse.estate.city")).sendKeys("Dresden");
-	    driver.findElement(By.id("cse.venture.originalFunding.equityCash")).clear();
-	    driver.findElement(By.id("cse.venture.originalFunding.equityCash")).sendKeys("0");
-	    driver.findElement(By.id("cse.venture.priceDue")).clear();
-	    driver.findElement(By.id("cse.venture.priceDue")).sendKeys("28.07.2017");
+	//Startseite is called and loaded in  Firefox
+	driver.get(baseURL); 
+	Assert.assertEquals(driver.getTitle(), "Baufinanzierung – das beste Darlehens-Angebot von 250 Banken - Commerzbank");  
 
-	    driver.findElement(By.xpath("//tr[@id='b2cAppButtonLine']/td[2]/a/span")).click();   
-	  }
-	  
-	  @Test
-	  public void testBaufinanzierung_Antrag_Zinsen_Check () throws Exception 
-	  {
-	    //Page : Baufinanzierung Onlineantrag - Zinsen Check
-	  
-	    assertEquals(driver.findElement(By.cssSelector("td.section")).getText(), "Zinscheck");
-	    driver.findElement(By.xpath("//tr[@id='b2cAppButtonLine']/td[2]/a/span")).click();
+	// Perform the click operation that opens new window
+	driver.findElement(By.cssSelector("li.flex-active-slide > a.center.top")).click();
+	
+	String parentWindow = driver.getWindowHandle();
+	
+	Set<String> handles =  driver.getWindowHandles();
+	   for(String windowHandle  : handles)
+	       {
+	       if(!windowHandle.equals(parentWindow))
+	          {
+	          driver.switchTo().window(windowHandle);
+	         //!--Perform your operation here for new window-->
+	         Formularausfuellen();    
+	         driver.close(); //closing child window
+	         driver.switchTo().window(parentWindow); //cntrl to parent window
+	          }
+	       }  
+  }
+
+public void Formularausfuellen ()
+{
+	
+	System.out.println("nach  for condt. winhandle");
+	
+	// Page : Baufinanzierung Onlineantrag -Start
+
+	WebDriverWait wait  = new WebDriverWait(driver,15);
+	wait.until(ExpectedConditions.presenceOfElementLocated(By.id("cse.venture.reason")));
+	
+	Assert.assertEquals("Commerzbank Finanzierungsanfrage", driver.getTitle());
+		
+	 new Select(driver.findElement(By.id("cse.venture.reason"))).selectByVisibleText("Kauf eines Neubaus vom Bauträger");
+	 new Select(driver.findElement(By.id("cse.estate.combiType"))).selectByVisibleText("Eigentumswohnung");
+	 new Select(driver.findElement(By.id("cse.estate.utilization"))).selectByVisibleText("selbst bewohnen");
+	 if (!driver.findElement(By.id("radioButton_cse.estate.notfound_0")).isSelected()) {
+	   driver.findElement(By.id("radioButton_cse.estate.notfound_0")).click();
+	 };
+	 new Select(driver.findElement(By.id("cse.estate.buyTimeline"))).selectByVisibleText("");
+	 driver.findElement(By.id("cse.estate.zip")).clear();
+	 driver.findElement(By.id("cse.estate.zip")).sendKeys("01097");
+	 driver.findElement(By.id("cse.venture.priceBuilding")).clear();
+	 driver.findElement(By.id("cse.venture.priceBuilding")).sendKeys("260000");
+	 driver.findElement(By.id("cse.venture.originalFunding.loans[1].amount")).clear();
+	 driver.findElement(By.id("cse.venture.originalFunding.loans[1].amount")).sendKeys("50000");
+	 new Select(driver.findElement(By.id("cse.venture.originalFunding.loans[1].maturity"))).selectByVisibleText("10 Jahre");
+	 if (!driver.findElement(By.name("amortType")).isSelected()) {
+	   driver.findElement(By.name("amortType")).click();
+	 };
 	 
-	  }
+	 driver.findElement(By.id("cse.venture.originalFunding.loans[1].amortisation")).clear();
+	 driver.findElement(By.id("cse.venture.originalFunding.loans[1].amortisation")).sendKeys("2");
 	  
-	  
-	  @Test
-	  public void testBaufinanzierung_Antrag_Persoenlichen_Daten () throws Exception 
-	  {
-	  
-	   // Page : Baufinanzierung Onlineantrag  - Persönlichen Daten
+	 driver.findElement(By.id("togglelinkoptionalOther")).click();
 	    
-	    assertEquals(driver.findElement(By.cssSelector("td.section")).getText(), "Persönliche Angaben");
-	    new Select(driver.findElement(By.id("cse.mainApplicant.sex"))).selectByVisibleText("Herr");
-	    driver.findElement(By.id("cse.mainApplicant.fname")).clear();
-	    driver.findElement(By.id("cse.mainApplicant.fname")).sendKeys("Roman");
-	    driver.findElement(By.id("cse.mainApplicant.lname")).clear();
-	    driver.findElement(By.id("cse.mainApplicant.lname")).sendKeys("Polanski");
-	    driver.findElement(By.id("cse.mainApplicant.birthdate")).clear();
-	    driver.findElement(By.id("cse.mainApplicant.birthdate")).sendKeys("02.02.1952");
-	    driver.findElement(By.id("cse.mainApplicant.zip")).clear();
-	    driver.findElement(By.id("cse.mainApplicant.zip")).sendKeys("01099");
-	    driver.findElement(By.id("cse.mainApplicant.city")).clear();
-	    driver.findElement(By.id("cse.mainApplicant.city")).sendKeys("Frankfurt am Main");
-	    driver.findElement(By.id("cse.mainApplicant.phoneEvening")).clear();
-	    driver.findElement(By.id("cse.mainApplicant.phoneEvening")).sendKeys("00497272727272");
-	    driver.findElement(By.id("cse.mainApplicant.email")).clear();
-	    driver.findElement(By.id("cse.mainApplicant.email")).sendKeys("rpolanski@frankfurt.de");
-	    new Select(driver.findElement(By.id("cse.mainApplicant.jobStatus"))).selectByVisibleText("Elternzeit");
-	    driver.findElement(By.id("cse.mainApplicant.netSalary")).clear();
-	    driver.findElement(By.id("cse.mainApplicant.netSalary")).sendKeys("45000");
-	    new Select(driver.findElement(By.cssSelector("select[name=\"cse.mainApplicant.netSalary\"]"))).selectByVisibleText("monatlich");
-	    driver.findElement(By.id("cse.venture.ventureDesc")).clear();
-	    driver.findElement(By.id("cse.venture.ventureDesc")).sendKeys("Einmal Test");
-	    if (!driver.findElement(By.id("radioButton_cse.venture.isCobaCustomer_0")).isSelected()) {
-	      driver.findElement(By.id("radioButton_cse.venture.isCobaCustomer_0")).click();
-	    };
-	    	    
-	    driver.findElement(By.id("cse.mainApplicant.contactDate")).clear();
-	    driver.findElement(By.id("cse.mainApplicant.contactDate")).sendKeys("02.09.2017");
- 
-	    // Close the new window, if that window no more required
-	    driver.close();
-
-	    // Switch back to original browser (first window)
-	    driver.switchTo().window(winHandleBefore);
-	  }
+	 driver.findElement(By.id("cse.estate.city")).clear();
+	 driver.findElement(By.id("cse.estate.city")).sendKeys("Dresden");
+	 driver.findElement(By.id("cse.venture.originalFunding.equityCash")).clear();
+	 driver.findElement(By.id("cse.venture.originalFunding.equityCash")).sendKeys("0");
+	 driver.findElement(By.id("cse.venture.priceDue")).clear();
+	 driver.findElement(By.id("cse.venture.priceDue")).sendKeys("28.07.2017");
+	
+	 driver.findElement(By.xpath("//tr[@id='b2cAppButtonLine']/td[2]/a/span")).click();
+	 
+	 //Page : Baufinanzierung Onlineantrag - Zinsen Check
+	
+	 Assert.assertEquals(driver.findElement(By.cssSelector("td.section")).getText(), "Zinscheck");
+	 driver.findElement(By.xpath("//tr[@id='b2cAppButtonLine']/td[2]/a/span")).click();
+	 
+	// Page : Baufinanzierung Onlineantrag  - Persönlichen Daten
+	 
+	 Assert.assertEquals(driver.findElement(By.cssSelector("td.section")).getText(), "Persönliche Angaben");
+	 new Select(driver.findElement(By.id("cse.mainApplicant.sex"))).selectByVisibleText("Herr");
+	 driver.findElement(By.id("cse.mainApplicant.fname")).clear();
+	 driver.findElement(By.id("cse.mainApplicant.fname")).sendKeys("Roman");
+	 driver.findElement(By.id("cse.mainApplicant.lname")).clear();
+	 driver.findElement(By.id("cse.mainApplicant.lname")).sendKeys("Polanski");
+	 driver.findElement(By.id("cse.mainApplicant.birthdate")).clear();
+	 driver.findElement(By.id("cse.mainApplicant.birthdate")).sendKeys("02.02.1952");
+	 driver.findElement(By.id("cse.mainApplicant.zip")).clear();
+	 driver.findElement(By.id("cse.mainApplicant.zip")).sendKeys("01099");
+	 driver.findElement(By.id("cse.mainApplicant.city")).clear();
+	 driver.findElement(By.id("cse.mainApplicant.city")).sendKeys("Frankfurt am Main");
+	 driver.findElement(By.id("cse.mainApplicant.phoneEvening")).clear();
+	 driver.findElement(By.id("cse.mainApplicant.phoneEvening")).sendKeys("00497272727272");
+	 driver.findElement(By.id("cse.mainApplicant.email")).clear();
+	 driver.findElement(By.id("cse.mainApplicant.email")).sendKeys("rpolanski@frankfurt.de");
+	 new Select(driver.findElement(By.id("cse.mainApplicant.jobStatus"))).selectByVisibleText("Elternzeit");
+	 driver.findElement(By.id("cse.mainApplicant.netSalary")).clear();
+	 driver.findElement(By.id("cse.mainApplicant.netSalary")).sendKeys("45000");
+	 new Select(driver.findElement(By.cssSelector("select[name=\"cse.mainApplicant.netSalary\"]"))).selectByVisibleText("monatlich");
+	 driver.findElement(By.id("cse.venture.ventureDesc")).clear();
+	 driver.findElement(By.id("cse.venture.ventureDesc")).sendKeys("Einmal Test");
+	 if (!driver.findElement(By.id("radioButton_cse.venture.isCobaCustomer_0")).isSelected()) {
+	   driver.findElement(By.id("radioButton_cse.venture.isCobaCustomer_0")).click();
+	 };
+	 
+	 
+	 driver.findElement(By.id("cse.mainApplicant.contactDate")).clear();
+	 driver.findElement(By.id("cse.mainApplicant.contactDate")).sendKeys("02.09.2017"); 
+}
 
 	@AfterClass
 	public static void tearDown() throws Exception 
@@ -165,41 +173,4 @@ public class TestCaculator
 	    if (!"".equals(verificationErrorString)) 
 	    	Assert.fail(verificationErrorString);
 	  }
-
-	  
-//	  private boolean isElementPresent(By by) 
-//	  {
-//	    try {
-//	      driver.findElement(by);
-//	      return true;
-//	    } catch (NoSuchElementException e) {
-//	      return false;
-//	    }
-//	  }
-//
-//	  private boolean isAlertPresent() 
-//	  {
-//	    try {
-//	      driver.switchTo().alert();
-//	      return true;
-//	    } catch (NoAlertPresentException e) {
-//	      return false;
-//	    }
-//	  }
-//
-//	  private String closeAlertAndGetItsText() 
-//	  {
-//	    try {
-//	      Alert alert = driver.switchTo().alert();
-//	      String alertText = alert.getText();
-//	      if (acceptNextAlert) {
-//	        alert.accept();
-//	      } else {
-//	        alert.dismiss();
-//	      }
-//	      return alertText;
-//	    } finally {
-//	      acceptNextAlert = true;
-//	    }
-//	  }
 }

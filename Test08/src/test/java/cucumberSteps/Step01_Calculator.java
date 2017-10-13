604432aeaea7b8085bb7ceb49223a99938e1b6db
Paul Zilewitsch <pzilewitsch@proficom.de>
2017-10-13 10:08:53 +0200

    lo

--- Test08/src/test/java/cucumberSteps/Step01_Calculator.java
+++ Test08/src/test/java/cucumberSteps/Step01_Calculator.java
@@ -179,4 +179,46 @@ public class Step01_Calculator
 //		if (!"".equals(verificationErrorString))
 //			Assert.fail(verificationErrorString);
 //	}
+	@Given ("^user navigates to Facebook$")
+	public void Open_firefox_and_start_application_fb() throws Throwable 
+	{
+//		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
+//		driver = new FirefoxDriver();
+		
+		nodeUrl = "http://172.16.20.161:4444/wd/hub";
+
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setBrowserName("firefox");
+		capabilities.setPlatform(Platform.WINDOWS);
+
+		driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
+		
+		baseURL = "https://www.facebook.com";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.get(baseURL);
+		assertEquals(driver.getTitle(), "Facebook – Anmelden oder Registrieren");
+		driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+	}
+		
+		@When ("^user logs in using Username as \"([^\"]*)\" and Password as \"([^\"]*)\"$")
+		
+		public void I_enter_Username_as_and_Password_as(String arg1, String arg2) {
+		   driver.findElement(By.id("email")).sendKeys(arg1);
+		   driver.findElement(By.id("pass")).sendKeys(arg2);
+		   driver.findElement(By.id("u_0_v")).click(); 
+		} 
+		
+		@Then("^login should be unsuccessful$")
+		public void validateRelogin() { 
+			   if(driver.getCurrentUrl().equalsIgnoreCase(
+			      "https://www.facebook.com/login.php?login_attempt=1&lwv=110")){ 
+			         System.out.println("Test Pass");
+			   } else { 
+			      System.out.println("Test Failed"); 
+			   } 
+			   driver.close(); 
+			}
 }
+
+
